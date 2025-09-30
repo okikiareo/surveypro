b68e828e4c4ed031b9e4fc597fcd101aec49bfc5
okikiareo <okikiareo@gmail.com>
2025-09-30 12:34:26 +0100

    /

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,5 @@
 const config = {
-  API_URL: "http://surveytools.onrender.com",
-  // API_URL: import.meta.env.VITE_API_URL || 'https://staging.surveyprotools.com',
-  // API_URL: 'https://staging.surveyprotools.com',
+  API_URL: import.meta.env.VITE_API_URL || "https://surveytools.onrender.com",
   PAYSTACK_PUBLIC_KEY: import.meta.env.VITE_PAYSTACK_PUBLIC_KEY
 };
 
